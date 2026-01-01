cd1556a59d5bf6ece44f273f699471ed91b80cfd
= <=>
2026-01-01 01:11:04 -0300

    fix dependency

--- modules/motd.nix
+++ modules/motd.nix
@@ -1,10 +1,10 @@
-{ hostName, flakeName, ... }:
+{ hostName, flakeName, config, ... }:
 
 {
   environment.etc."motd".text = ''
     ${flakeName}
     Host: ${hostName}
-    System: ${pkgs.stdenv.hostPlatform.system}
+    System: ${config.nixpkgs.hostPlatform.system}
     Kernel: ${config.boot.kernelPackages.kernel.version}
     Generation: ${config.system.stateVersion}
   '';
