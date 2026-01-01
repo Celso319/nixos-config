b615e238a3041c9d10ed5f5f4b37c6edb8991ac0
= <=>
2026-01-01 00:57:35 -0300

    Expand motd

--- modules/motd.nix
+++ modules/motd.nix
@@ -4,5 +4,8 @@
   environment.etc."motd".text = ''
     ${flakeName}
     Host: ${hostName}
+    System: ${pkgs.stdenv.hostPlatform.system}
+    Kernel: ${config.boot.kernelPackages.kernel.version}
+    Generation: ${config.system.stateVersion}
   '';
 }
