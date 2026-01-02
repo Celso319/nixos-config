ca4c50304d4e0e8fbcc70c94d997776b4837817f
= <=>
2026-01-02 01:59:35 -0300

    remove boot config

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,14 +1,1 @@
-{ config, pkgs, ... }:
-
-{
-  # Headless / no HDMI
-  services.xserver.enable = false;
-
-  console.enable = true;
-
-  systemd.defaultUnit = "multi-user.target";
-
-  boot.kernelParams = [
-    "nomodeset"
-  ];
-}
+{}
