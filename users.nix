22abf9df05302117267ca32223f4b11f86e8778b
Celso Arthur <celo_arthur@hotmail.com>
2026-03-21 20:37:28 -0300

    Add ssh key to users.nix

--- users.nix
+++ users.nix
@@ -19,6 +19,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyJ/SLfhM+dGtUrojKlJOaFcz1vMidPkPjuuF0lzPEd celso@pc"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+aZ/jsGMAfMk3seyObhgpoar0/apdHI6OW9moKRgP0 celso@debiannote"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxHVCnuPX1F2Ekvi2+bBn82PQdJGbno7G36Jh4YPIgV celso@debian-note-home"
     ];
   };
 }
